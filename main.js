d74a66243803cdfcfb79411a4ed079f695c42ed0
Martín Corti <121203914+MartinCorti@users.noreply.github.com>
2023-05-17 23:17:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+calcular el valor de un producto en cuotas
+
+primero precio de contado
+
+luego precio en cuotas
+
+    luego interés
+    luego inflación
+    finalmente cantidad de cuotas
+
+si precio de contado < precio en cuotas => precio de contado
+si precio de contado > precio en cuotas => precio en cuotas
+
+// ENTRADA DE DATOS > PROCESAMIENTO DE DATOS > SALIDA (O INFORMACIÓN)
+// Etapa 1 - Entrada de Datos
+// Solicito el Monto de Contado
+
+let monto = 0;
+
+while ((monto <= 0))
+
+// Realizamos los Cálculos
+function calculoDePrecioCuotas(mon)
